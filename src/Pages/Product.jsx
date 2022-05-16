b5861b12a27155351dafb1884239d93c1e414bdb
nachatoui <91183220+nachatoui@users.noreply.github.com>
2022-05-16 15:16:08 +0200

    Update Product.jsx

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,7 +2,6 @@ import styled from "styled-components"
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import Newsletter from "../components/Newsletter";
 import { Add, Remove } from "@mui/icons-material";
 import {mobile} from "../responsive";
 import { useLocation } from "react-router-dom";
@@ -191,7 +190,6 @@ const Product = () => {
                 </AddContainer>
             </InfoContainer>
         </Wrapper>
-        <Newsletter/>
         <Footer/>
     </Container>
   )
