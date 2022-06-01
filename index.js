dc4d99b53ce9911f9b47343aa3175b1041d5511b
nachatoui <91183220+nachatoui@users.noreply.github.com>
2022-06-01 13:49:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,4 +20,11 @@ app.use("/api/products", productRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
+  
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
 });
