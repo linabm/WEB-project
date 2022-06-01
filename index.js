b0e609e09817e9c11db7505d3008056c4b168968
nachatoui <91183220+nachatoui@users.noreply.github.com>
2022-06-01 16:57:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/products", productRoute); 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
   
 });
